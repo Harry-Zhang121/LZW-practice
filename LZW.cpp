a358238548277b5d3234b40b82b678efdc6bb0d1
Harry Zhang <nonaharry@yeah.net>
2020-05-12 16:46:46 +0200

    Compress module is working but need some polish

--- LZW.cpp
+++ LZW.cpp
@@ -4,11 +4,10 @@
 
 LZW::LZW()
 {
-    std::cout << "ini";
-    std::vector<struct node> Dict(1);
+    // std::vector<Node> Dict(0);
     for(int i=0; i<=255; i++)
     {
-        Dict.push_back(node::make_node(-1,i));
+        Dict.push_back(Node(-1, i));
         // Dict[i] = node{1, i};
         //Dict[i].previous = -1;
         //Dict[i].current = i;
@@ -22,9 +21,17 @@ LZW::~LZW()
     Dict.clear();
 }
 
+void LZW::p_test()
+{
+    for(int i = 0; i<255; i++)
+    {
+        std::cout << Dict[i].current << std::endl;
+    }
+}
+
 void LZW::compress(std::ifstream &instream, std::ofstream &outstream)
 {
-    int word;
+    char word;
 
     // FILE *fpin = NULL;
     // fpin = fopen(inpath, "r");
@@ -36,6 +43,7 @@ void LZW::compress(std::ifstream &instream, std::ofstream &outstream)
     {
         // Read one character from the input file.
         instream >> word;
+        std::cout << "Now reading:" << word << std::endl;
         // Break if reached the end.
         if(instream.eof())
             break;
@@ -47,7 +55,7 @@ void LZW::compress(std::ifstream &instream, std::ofstream &outstream)
         for(int i=0; i<ct; i++)
         {
             // If in the Dicttionary, p = the index of Dicttionary.
-            if(Dict[i].previous == p && Dict[i].current == word)
+            if(Dict[i].previous == p && Dict[i].current == (int)word)
             {
                 p = i;
                 sw = 1;
@@ -57,13 +65,14 @@ void LZW::compress(std::ifstream &instream, std::ofstream &outstream)
         // If not in the Dicttionary, create a new Dicttionary element for the word.
         if(!sw)
         {
-            Dict[ct].previous = p;
-            Dict[ct].current = word;
+            Dict.push_back(Node(p,word));
+            // Dict[ct].previous = p;
+            // Dict[ct].current = word;
 
             //output p in 4 digits hex format. 2^16 = 16^4 = 65536
-            outstream << p << '/n';
+            outstream << p << std::endl;
             //fprintf(fpout, "%04X", p);
-            p = word;
+            p = (int)word;
             ct++;
       }
     }
--- LZW.h
+++ LZW.h
@@ -2,25 +2,25 @@
 #include<vector>
 #include<fstream>
 
-class node
+class Node
 {
     public:
-    int previous;
-    int current;
+        int previous;
+        int current;
+        Node(int prev, int curr): previous(prev), current(curr) {};
     
-    static node make_node(int pre, int cur)
-    {
-        class node temp;
-        temp.previous = pre;
-        temp.current = cur;
-        return temp;
-    }
+    // static node make_node(int pre, int cur)
+    // {
+    //     class node temp;
+    //     temp.previous = pre;
+    //     temp.current = cur;
+    //     return temp;
+    // }
 };
 
 class LZW
 {
-    private:
-    std::vector<struct node> Dict;
+    std::vector<Node> Dict;
     int ct;
     int p;
 
@@ -28,6 +28,7 @@ class LZW
     LZW();      //Initialize the dictionary
     ~LZW();
 
+    void p_test();
     void compress(std::ifstream &instream, std::ofstream &outstream);
     void decompress(char *inpath, char *outpath);
 
